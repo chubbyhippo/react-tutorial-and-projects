abf09dfc1397561b38c6b6ff7483e380d2feccd2
ch <mkmsacc@outlook.com>
2021-01-10 21:45:55 +0700

    Finish 32

---
+++ section03/tutorial/src/books.js
@@ -0,0 +1,25 @@
+const books = [
+    {
+      id: 1,
+      img:
+        "https://images-na.ssl-images-amazon.com/images/I/81vpsIs58WL._AC_UL200_SR200,200_.jpg",
+      title: "Love You Forever",
+      author: "Robert Munsch",
+    },
+    {
+      id: 2,
+      img:
+        "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
+      title: "If Animals Kissed Good Night",
+      author: "Ann Whitford Paul",
+    },
+    {
+      id: 3,
+      img:
+        "https://images-na.ssl-images-amazon.com/images/I/910lu-HKDYL._AC_UL200_SR200,200_.jpg",
+      title: "Little Blue Truck's Valentine",
+      author: "Alice Schertle",
+    },
+  ];
+
+export default books
---
+++ section03/tutorial/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+// CSS
+import "./index.css";
+import data from "./books";
+import Book from "./Book";
+import { greeting } from "./testing/testing";
+
+function BookList() {
+  console.log(greeting);
+  return (
+    <section className="booklist">
+      {data.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById("root"));
