dda554251fb9e72d153df1c87b52086c3a9af7b6
ch <mkmsacc@outlook.com>
2021-01-28 09:00:13 +0700

    Add 2-useEffect-cleanup

--- section04/tutorial/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ section04/tutorial/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("userEffect");
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+  console.log("render");
+  return (
+    <React.Fragment>
+      <h1>window</h1>
+      <h2>{size} px</h2>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectCleanup;
