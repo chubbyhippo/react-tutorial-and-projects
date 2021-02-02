acc746584e301733fc4a4543da2fd8ed3d121aaa
ch <mkmsacc@outlook.com>
2021-02-02 10:14:25 +0700

    Update

--- section04/tutorial/src/tutorial/3-conditional-rendering/setup/2-multiple-returns.js
+++ section04/tutorial/src/tutorial/3-conditional-rendering/setup/2-multiple-returns.js
@@ -5,22 +5,45 @@ const MultipleReturns = () => {
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState("default user");
 
-  setInterval(() => {
-    setIsLoading(false);
-  }, 2000);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 299) {
+          return res.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(res.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   if (isLoading) {
-    return <div>
-      <h1>Loading...</h1>
-    </div>;
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
   }
-  
+
   if (isError) {
-    return <div>
-      <h1>Error...</h1>
-    </div>;
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
   }
-  return <h2>multiple returns</h2>;
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
